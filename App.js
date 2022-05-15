e29d6db012a9e9120a40fbc58f6db903b1fe97fb
fierce962 <xalbinox2@gmail.com>
2022-05-15 19:14:53 -0400

    implements drawer navigation

--- App.js
+++ App.js
@@ -3,14 +3,11 @@ import React from 'react';
 import Main from './src/main';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store'
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
   return (
     <Provider store={ store }>
-      <GestureHandlerRootView style={{ flex: 1 }}>
         <Main />
-      </GestureHandlerRootView>
     </Provider>
   );
 };
--- src/main.js
+++ src/main.js
@@ -1,73 +1,50 @@
 import React from 'react';
-import { Button, View } from 'react-native'
+import { Button, View, DrawerLayoutAndroid, Text  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import ViewPronunciation from './components/ViewPronunciation';
 import ViewWordel from './components/ViewWordel';
 import Home from './Views/home';
 import IconTouchable from './components/IconTouchable';
 
-function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button
-            onPress={() => navigation.navigate('Notifications')}
-            title="Go to notifications"
-            />
-        </View>
-    );
-}
-
-function NotificationsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button onPress={() => navigation.goBack()} title="Go back home" />
-        </View>
-    );
-}
-
-const Drawer = createDrawerNavigator();
-
 export default function Main() {
     const Stack = createNativeStackNavigator();
-
-    // return (
-    // <NavigationContainer>
-    //     <Drawer.Navigator initialRouteName="Home">
-    //         <Drawer.Screen name="Home" component={HomeScreen} />
-    //         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-    //     </Drawer.Navigator>
-    // </NavigationContainer>
-    // );
+    var navigationView = (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+            <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+        </View>
+        );
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Group screenOptions={({ navigation, route }) => ({
-                        headerTitle: 'MrOwl',
-                        headerTitleAlign: 'center',
-                        headerStyle: {
-                            backgroundColor: '#181818'
-                        },
-                        headerTitleStyle: {
-                            color: '#fff'
-                        },
-                        contentStyle: {
-                            backgroundColor: '#181818',
-                            borderTopWidth: 0.6,
-                            borderTopColor: '#fff'
-                        },
-                        // headerLeft: () => (
-                        //     <IconTouchable props={ { navigation, route } }/>
-                        // )
-                    })}>
-                    <Stack.Screen name='home' component={ Home } />
-                    <Stack.Screen name='wordel' component={ ViewWordel } />
-                    <Stack.Screen name='voice' component={ ViewPronunciation } />
-                </Stack.Group>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <DrawerLayoutAndroid
+                drawerWidth={300}
+                renderNavigationView={() => navigationView}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Group screenOptions={({ navigation, route }) => ({
+                            headerTitle: 'MrOwl',
+                            headerTitleAlign: 'center',
+                            headerStyle: {
+                                backgroundColor: '#181818'
+                            },
+                            headerTitleStyle: {
+                                color: '#fff'
+                            },
+                            contentStyle: {
+                                backgroundColor: '#181818',
+                                borderTopWidth: 0.6,
+                                borderTopColor: '#fff'
+                            },
+                            // headerLeft: () => (
+                            //     <IconTouchable props={ { navigation, route } }/>
+                            // )
+                        })}>
+                        <Stack.Screen name='home' component={ Home } />
+                        <Stack.Screen name='wordel' component={ ViewWordel } />
+                        <Stack.Screen name='voice' component={ ViewPronunciation } />
+                    </Stack.Group>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </DrawerLayoutAndroid>
     );
 }
