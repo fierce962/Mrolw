a60848da22f15dacd0807ed560147e727e8749d3
fierce962 <xalbinox2@gmail.com>
2022-05-19 17:53:41 -0400

    change

--- src/models/ControllerNotifications.js
+++ src/models/ControllerNotifications.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Notifications } from 'react-native-notifications';
 
 class ControllerNotifications{
     createNotification(){
         // - 30 minutos 1.8e+6
         console.log(new Date(new Date().getTime() + 120000))
-        Notifications.postLocalNotification({
-            body: "Local notification!",
-            title: "Local Notification Title",
-            sound: "chime.aiff",
-            silent: false,
-            category: "SOME_CATEGORY",
-            userInfo: { },
-            fireDate: new Date(new Date().getTime() + 120000),
-        });
     }
 }
 
