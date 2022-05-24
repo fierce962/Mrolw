e516d2f94c1949edc66d3998d4734e2e141cc213
fierce962 <xalbinox2@gmail.com>
2022-05-24 07:42:17 -0400

    timer animated

--- src/components/CreateTimerCount.js
+++ src/components/CreateTimerCount.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Animated } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { calcTime } from '../features/timerCount/timerCountSlice';
+import { restTime } from '../features/timerCount/timerCountSlice';
 
 export default function CreateTimerCount(){
     const dispatch = useDispatch();
     const goalDate = useSelector(state => state.timer.goalDate);
-    const time = useSelector(state => state.timer.timeRemaining)
+    const time = useSelector(state => state.timer.timeRemaining);
+
+    let animate = new Animated.Value(0);
+    Animated.timing(animate, {
+        toValue: -40,
+        duration: 1000,
+        useNativeDriver: false,
+        delay: 3000
+    }).start();
 
     if(time >= 0){
         setTimeout(() => {
             console.log('time out');
-            dispatch(calcTime())
-        }, 60000);
+            Animated.timing(animate, {
+                toValue: 0,
+                duration: 0,
+                useNativeDriver: false,
+            }).start();
+            dispatch(restTime());
+        }, 4000);
     }else{
         console.log('ya no mas')
     }
 
+    const fisrtNumber = [
+        style.viewAnimation,
+        time[1] - 1 < 0 && { bottom: animate },
+        time[1] - 1 >= 0 && { bottom: 0 }
+    ]
+    
     return (
-        <View>
-            <Text style={ {color: '#fff' } }>{ time }</Text>
+        <View style={ style.contentTimer }>
+            <View style={ style.contentText }>
+                <Animated.View style={ fisrtNumber }>
+                    <Text style={ style.textTimer }>{ time[0] - 1 }</Text>
+                    <Text style={ style.textTimer }>{ time[0] }</Text>
+                </Animated.View>
+            </View>
+            <View style={ style.contentText }>
+                <Animated.View style={[ style.viewAnimation, { bottom: animate } ]}>
+                    <Text style={ style.textTimer }>{ time[1] === '0' ? 9 : time[1] - 1 }</Text>
+                    <Text style={ style.textTimer }>{ time[1] }</Text>
+                </Animated.View>
+            </View>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    contentTimer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    contentText: {
+        width: 25,
+        height: 40,
+        alignItems: 'center',
+        backgroundColor: 'red',
+        position: 'relative',
+        overflow: 'hidden'
+    },
+    viewAnimation: {
+        position: 'absolute',
+    },  
+    textTimer: {
+        fontSize: 30,
+        color: '#fff'
+    }
+})
--- src/features/timerCount/timerCountSlice.js
+++ src/features/timerCount/timerCountSlice.js
@@ -3,17 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const timerCounter = createSlice({
     name: 'timer',
     initialState: {
-        goalDate: '2022-05-22T04:50:25.700Z',
+        goalDate: '2022-05-24T11:56:28.312Z',
         timeRemaining: undefined
     },
     reducers: {
         calcTime(state){
             console.log('calctime')
             let actualDate = new Date();
-            state.timeRemaining = parseInt((new Date(state.goalDate).getTime() - actualDate.getTime()) / 60000)
+            state.timeRemaining = parseInt((new Date(state.goalDate).getTime() - actualDate.getTime()) / 60000).toString();
+        },
+        restTime(state){
+            if(parseInt(state.timeRemaining) - 1 >= 10){
+                state.timeRemaining = (parseInt(state.timeRemaining) - 1).toString();
+            }else{
+                state.timeRemaining = `0${ parseInt(state.timeRemaining) - 1 }`;
+            }
         }
     }
 });
 
 export default timerCounter.reducer;
-export const { calcTime } = timerCounter.actions;
+export const { calcTime, restTime } = timerCounter.actions;
