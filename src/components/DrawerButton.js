924e1a182f16df18f26f8404eaed4867b7f9c4b8
fierce962 <xalbinox2@gmail.com>
2022-06-08 12:10:15 -0400

    add items in drawer

--- src/components/DrawerButton.js
+++ src/components/DrawerButton.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { View, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { controllerNavigation } from "../models/ControllerNavigation";
 
 import ProfileImg from "./ProfileImg";
+import CreateButton from '../components/CreateButton';
 
 export default function DrawerButton({ drawer }) {
     return (
         <View style={[ { flex: 1 }, { backgroundColor: '#202020' } ]}>
             <ProfileImg profileName={ 'fierce' } />
+            <CreateButton title={ 'Perfil' } aditionalStyle={ style.changeBtnStyle } />
+            <CreateButton title={ 'Palabras' } aditionalStyle={ style.changeBtnStyle } />
+            <CreateButton title={ 'Oraciones' } aditionalStyle={ style.changeBtnStyle } />
+            <CreateButton title={ 'Prueba con audio' } aditionalStyle={ style.changeBtnStyle } />
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    changeBtnStyle: {
+        marginTop: 5
+    }
+});
